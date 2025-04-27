8a05b240719687c6c9443f90b23a06ca5251b33d
heitor <heitor.nolla@gmail.com>
2025-04-27 11:11:05 -0300

    finished crawler config

--- extraction/extraction/spiders/mercadolivre.py
+++ extraction/extraction/spiders/mercadolivre.py
@@ -5,10 +5,14 @@ class MercadoLivreSpider(scrapy.Spider):
     allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/baixo-5-cordas"]
 
+    page_count = 1
+    max_pages = 10
+
     def parse(self, response):
         products = response.css('div.ui-search-result__wrapper')
 
         for product in products:
+            # Mercado Livre stores multiple "prices" in this single span
             prices = product.css('span.andes-money-amount__fraction::text').getall()
 
             yield {
@@ -19,4 +23,9 @@ class MercadoLivreSpider(scrapy.Spider):
                 'reviews_rating_number': product.css('span.poly-reviews__rating::text').get(),
                 'reviews_amount': product.css('span.poly-reviews__total::text').get()
             }
-        pass
+
+        if self.page_count < self.max_pages:
+            next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+            if next_page:
+                self.page_count += 1
+                yield scrapy.Request(url=next_page, callback=self.parse)
