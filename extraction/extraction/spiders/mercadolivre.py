c1635cfebf1faa9d47e7cdf6059a59c5793275ef
heitor <heitor.nolla@gmail.com>
2025-04-27 16:19:02 -0300

    Changed spider

--- extraction/extraction/spiders/mercadolivre.py
+++ extraction/extraction/spiders/mercadolivre.py
@@ -6,7 +6,7 @@ class MercadoLivreSpider(scrapy.Spider):
     start_urls = ["https://lista.mercadolivre.com.br/baixo-5-cordas"]
 
     page_count = 1
-    max_pages = 10
+    max_pages = 20
 
     def parse(self, response):
         products = response.css('div.ui-search-result__wrapper')
@@ -18,8 +18,7 @@ class MercadoLivreSpider(scrapy.Spider):
             yield {
                 'name': product.css('a.poly-component__title::text').get(),
                 'seller': product.css('span.poly-component__seller::text').get(),
-                'old_price':prices[0] if len (prices) > 0 else None, 
-                'new_price':prices[1] if len (prices) > 1 else None, 
+                'price':prices[0] if len (prices) > 0 else None, 
                 'reviews_rating_number': product.css('span.poly-reviews__rating::text').get(),
                 'reviews_amount': product.css('span.poly-reviews__total::text').get()
             }
