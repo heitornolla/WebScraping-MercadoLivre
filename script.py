13dd595241caf32379726f9de75e59766cffa0d9
heitor <heitor.nolla@gmail.com>
2025-04-27 17:21:00 -0300

    adjusted script

--- script.py
+++ script.py
@@ -3,7 +3,10 @@ import datetime
 
 def main():
   os.chdir('extraction/extraction/')
-  os.system(f'scrapy crawl mercadolivre -o ../../data/data-{datetime.date.today()}.json')
+  os.system(f'scrapy crawl mercadolivre -o ../../data/data.json')
+  os.chdir('../../transforms')
+  os.system('python data_transformation.py')
+
 
 if __name__ == "__main__":
   main()
