1da54ad8da6c365aa9e0f24d2e0e797dffb5fda0
heitor <heitor.nolla@gmail.com>
2025-04-29 14:07:41 -0300

    fixed path issue on script

--- script.py
+++ script.py
@@ -6,11 +6,11 @@ from transforms.data_transformation import transform_data
 
 def main():   
     # Check if data.json exists
-    data_path = os.path.abspath('../../data/data.json')
+    data_path = os.path.abspath('data/data.json')
 
     if os.path.exists(data_path):      
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        new_name = f"../../data/data_{timestamp}.json"
+        new_name = f"data/data_{timestamp}.json"
         os.rename(data_path, new_name)
         print(f"Renamed existing data.json to data_{timestamp}.json")
     
