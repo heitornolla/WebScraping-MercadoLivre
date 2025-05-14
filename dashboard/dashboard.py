a27426ab5e1d940337bcba7a62bd39b8d26788e6
heitor <heitor.nolla@gmail.com>
2025-05-13 22:59:30 -0300

    improved dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -19,17 +19,30 @@ def get_df_from_db(db_path: str) -> pd.DataFrame:
 
 
 def get_dashboard(df: pd.DataFrame):
-  st.title("Scraping Results from Mercado Livre")
-  st.subheader("By Heitor Nolla")
+    st.title("Scraping Results from Mercado Livre")
+    st.subheader("By Heitor Nolla")
 
-  col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-  total_itens = df.shape[0]
+    total_itens = df.shape[0]
+    col1.metric(label="Total Items Found", value=total_itens)
 
-  col1.metric(label="Total Items Found", value=total_itens)
+    avg_price = df['price'].mean()
+    col2.metric(label="Average BRL Price", value=f"{avg_price:.2f}")
 
-  avg_price = df['price'].mean()
-  col2.metric(label="Average BRL Price", value=f"{avg_price:.2f}")
+    st.markdown("### All Items")
+
+    # Search input
+    search_term = st.text_input("Search for a bass (or any keyword):")
+
+    # Filter dataframe based on search input
+    if search_term:
+        filtered_df = df[df.apply(lambda row: row.astype(str).str.contains(search_term, case=False).any(), axis=1)]
+    else:
+        filtered_df = df
+
+    st.dataframe(filtered_df.reset_index(drop=True))
+
 
 
 if __name__ == "__main__":
